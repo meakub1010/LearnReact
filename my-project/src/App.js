9410de74a648c32b806befe757ed60e4b208c659
Muhammad Eakub <meakub86@gmail.com>
2022-03-13 18:34:35 -0400

    learned event handling in functional and class based components

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -5,15 +5,18 @@ import Message from './components/Message';
 import Profile from './components/Profile';
 import Counter from './components/Counter';
 import Resume from './components/Resume';
+import FunctionEvent from './components/FunctionEvent';
+import ClassEvent from './components/ClassEvent';
 
 function App() {
   return (
     <div className="App">
       {/* <Message messageContent="This is message content" /> */}
       {/* <Profile name="Muhammad" lastname="Eakub" /> */}
-      <Resume name="Muhammad Eakub" />
+      {/* <Resume name="Muhammad Eakub" /> */}
       {/* <Counter></Counter> */}
-
+      {/* <FunctionEvent /> */}
+      <ClassEvent />
     </div>
   );
 }
---
+++ my-project/src/components/FunctionEvent.js
@@ -0,0 +1,21 @@
+
+
+function FunctionEvent(){
+
+    // function handleClick(){
+    //     console.log("Button is clicked!");
+    // }
+
+    const handleClick = () => {
+        console.log("Button is clicked!");
+    };
+
+    return(
+        <div>
+            Functional Component
+            <button onClick={handleClick}>Click Here</button>
+        </div>
+    )
+}
+
+export default FunctionEvent;
