2923c972c0742fecfcdd90a19f784ff7dc439917
Muhammad Eakub <meakub86@gmail.com>
2022-03-13 17:45:20 -0400

    added different type for components and props in react component

---
+++ my-project/src/components/Hello.js
@@ -0,0 +1,20 @@
+export default Hello;
+const name = "Muhammad"
+
+const displayMessage = () => {
+    return "I need help.";
+};
+
+function Hello(){
+    return <><h1>Hello {name}!</h1><h4>{displayMessage()}</h4></>;
+
+}
+
+
+// ES6 code to create same component as above
+
+//const Hello = () => <h1>Hello World 2!</h1>
+
+
+
+
---
+++ my-project/src/components/Message.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+class Message extends Component {
+
+
+    render(){
+        return <h2>Hello form class component!! {this.props.messageContent}</h2>
+    }
+}
+
+export default Message;
+
+
+// JSX - Javascript XML
